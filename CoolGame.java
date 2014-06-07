8b7bd6229bbc259ac7d33ddb876a6451a415b2ba
monty816 <tommyboymadden@yahoo.com>
2014-06-07 16:59:39 -0500

    JFrame works

--- CoolGame.java
+++ CoolGame.java
@@ -1,3 +1,6 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by tommy_000 on 6/7/2014.
  */
@@ -8,10 +11,18 @@ public class CoolGame {
         loz.startGame();
     }
 
+    private JFrame frame;
+
     public CoolGame() {}
 
     public void create() {
-
+        frame = new JFrame("Legend Of Zelda!");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(new Dimension(806, 635));
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.validate();
     }
 
     public void startGame() {
