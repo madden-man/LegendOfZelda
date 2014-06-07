faf07fccee91298de8ceb952a11543eecc2ed9e5
monty816 <tommyboymadden@yahoo.com>
2014-06-07 17:11:22 -0500

    initialize in private final for oldage and reproduction

--- TestEnemy.java
+++ TestEnemy.java
@@ -2,11 +2,10 @@
  * Created by Jimmy on 6/7/2014.
  */
 public class TestEnemy {
-    protected static final int OLDAGE;
-    protected static final int REPRODUCTIONAGE;
+    protected static final int OLDAGE = 80;
+    protected static final int REPRODUCTIONAGE = 20;
+
     public TestEnemy()
     {
-        REPRODUCTIONAGE = 20;
-        OLDAGE = 80;
     }
 }
