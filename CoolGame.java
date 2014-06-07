4727b27b3e37d9203466ccf69d7ba5b1b54268da
monty816 <tommyboymadden@yahoo.com>
2014-06-07 16:01:42 -0500

    I changed to CoolGame

---
+++ CoolGame.java
@@ -0,0 +1,21 @@
+/**
+ * Created by tommy_000 on 6/7/2014.
+ */
+public class CoolGame {
+    public static void main(String[] args) {
+        CoolGame loz = new CoolGame();
+        loz.create();
+        loz.startGame();
+    }
+
+    public CoolGame() {}
+
+    public void create() {
+
+    }
+
+    public void startGame() {
+
+    }
+
+}
