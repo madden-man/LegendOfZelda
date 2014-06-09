7c5ed332445cb36afdd0edcd04d797100a03fbc9
NobleGoat <avinash050411@gmail.com>
2014-06-09 16:45:06 -0700

    Removed Click and implemented MouseListener within StartMenu. This is because different MouseListener is needed for each JPanel so implementing them within their specific JPanel class is the most efficient method. In addition, CoolGame is added as a parameter to StartMenu so CoolGame methods can be executed from within StartMenu. These chanes reflect the approach that I will take to the remaining JPanel classes in the future.

--- CoolGame.java
+++ CoolGame.java
@@ -6,11 +6,14 @@ import java.awt.*;
 public class CoolGame {
     private JFrame frame;
     private StartMenu start;
+  //private Credits cred;
+  //private ChooseSave save;
+  //private Options opt;
   //private GamePane game;
     public static void main(String[] args) {
         CoolGame loz = new CoolGame();
         loz.create();
-        loz.startGame();
+        loz.startGame(1);
     }
 
     public CoolGame() {}
@@ -22,12 +25,16 @@ public class CoolGame {
         frame.setSize(new Dimension(806, 635));
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
-        start = new StartMenu();
+        start = new StartMenu(this);
+        start.addMouseListener(start);
         frame.add(start);
         frame.repaint();
     }
+    public void chooseSave() {
 
-    public void startGame() {
+    }
+
+    public void startGame(int numOfSave) {
 
     }
 
