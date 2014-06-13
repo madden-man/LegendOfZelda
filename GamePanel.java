b4d90fc73c4f62082fcb5377b67ee3a762e6be49
jimmy liang <liang.jimmy12@gmail.com>
2014-06-12 21:37:59 -0700

    ignore this

--- GamePanel.java
+++ GamePanel.java
@@ -8,6 +8,7 @@ import java.awt.*;
 public class GamePanel extends JPanel {
     private CoolGame game;
     private Image background;
+    private int h;
     public GamePanel(CoolGame cool)
     {
         background = new ImageIcon("background.png").getImage();
@@ -17,6 +18,7 @@ public class GamePanel extends JPanel {
         setMaximumSize(size);
         setSize(size);
         this.game = cool;
+        h=100;
     }
     public void start()
     {
@@ -24,10 +26,32 @@ public class GamePanel extends JPanel {
         frame.getContentPane().removeAll();
         frame.add(this);
         frame.revalidate();
+        act();
     }
 
     public void paintComponent(Graphics g) {
-        g.clearRect(0, 0, 500, 500);
-        g.drawImage((new Player("player.png")).getImg(), 100, 100, null);
+  //      g.clearRect(0, 0, 500, 500);
+        g.drawImage((new Player("player.png")).getImg(), h, h, null);
+        System.out.print("paint)");
+    }
+
+    public void act()
+    {
+        boolean done = false;
+        while(!done)
+        {
+            try {
+                h = 200;
+                repaint(100);
+                Thread.sleep(5000);
+                System.out.print("hre");
+            }
+            catch(InterruptedException e) {
+                e.getMessage();
+            }
+        }
+        //h = 100;
+       // repaint();
+   //     h = 300;
     }
 }
