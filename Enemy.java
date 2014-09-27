f85a7fb64f72a83681fcaa078786664baea2add5
jimmy liang <liang.jimmy12@gmail.com>
2014-09-27 15:20:21 -0700

    made enemy class. extends living being. made test enemy extend enemy class.

---
+++ Enemy.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Jimmy on 6/21/2014.
+ */
+public class Enemy extends LivingBeing {
+    protected Body prey;
+    public Enemy(String name, int x, int y)
+    {super(name, x, y);}
+}
--- TestEnemy.java
+++ TestEnemy.java
@@ -1,11 +1,21 @@
 /**
  * Created by Jimmy on 6/7/2014.
  */
-public class TestEnemy {
+public class TestEnemy extends Enemy {
     protected static final int OLDAGE = 80;
     protected static final int REPRODUCTIONAGE = 20;
+    protected static final int normalSpeed = 5;
 
     public TestEnemy()
     {
+    }
+    public void act()
+    {
+        velocity[X] = 0;
+        velocity[Y] = 0;
+        velocity[X] += acceleration[X];
+        velocity[Y] += acceleration[Y];
+        position[X] += velocity[X];
+        position[Y] += velocity[Y];
     }
 }
